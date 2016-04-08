f29018d6f1d19d17c9f79bbc2952a54c0da44b6e
ALLIAUME Hugo <kocal@live.fr>
2016-04-08 12:15:55 +0200

    La modalbox ouais !

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,23 +7,55 @@ function App() {
 
 
     self._initMap();
+    self._initModal();
 }
 
 App.prototype._initMap = function () {
     var self = this;
 
-    self._map = L.map('map-container').setView([51.505, -0.09], 13);
+    self._map = L.map('map-container').setView([45.9890156384, 4.71865778622], 13);
 
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(self._map);
 }
 
+App.prototype._initModal = function () {
+    var self = this;
+
+    self.$overlay = $('.overlay');
+    self.$popup = $('.popup');
+
+    self.hideModal(false);
+};
 
 App.prototype.run = function () {
     var self = this;
 
-    self.askForG9N(self.cbG9NSuccess, self.G9NFailure)
+    self.displayCheese();
+    // self.askForG9N(self.cbG9NSuccess, self.G9NFailure)
+};
+
+App.prototype.displayCheese = function () {
+    var self = this;
+
+    self.showModal();
+};
+
+App.prototype.showModal = function () {
+    var self = this;
+
+    self.$overlay.fadeIn(500);
+    self.$popup.fadeIn(1000);
+};
+
+App.prototype.hideModal = function (isAnimate) {
+    var self = this;
+
+    var timing = isAnimate ? 300 : 0;
+
+    self.$overlay.fadeOut(timing);
+    self.$popup.fadeOut(timing);
 };
 
 App.prototype.askForG9N = function (cbSuccess, cbFailure) {

2b5060081049c3ad626c2b1d3bde2113a2e1f718
ALLIAUME Hugo <kocal@live.fr>
2016-04-08 11:44:50 +0200

    Ahahah OpenLayers c'est vraiment trop bien :-))

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,8 +3,30 @@
  */
 
 function App() {
+    var self = this;
 
+    // self._map = L.map("map-container");
+
+    self._initMap();
 }
+
+App.prototype._initMap = function () {
+    var self = this;
+
+    self._map = new ol.Map({
+        target: 'map-container',
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.MapQuest({layer: 'sat'})
+            })
+        ],
+        view: new ol.View({
+            center: new ol.proj.fromLonLat([0, 0]),
+            zoom: 2
+        }),
+    });
+}
+
 
 App.prototype.run = function () {
     var self = this;
